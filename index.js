35c8af97883a54e96da3837008835a084f643a49
Shahed Salehian <shahed.salehian@gmail.com>
2021-10-06 20:18:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const sum = require("./sum")
+const multiply = require("./multiply")
+
+const sum_result = sum(3,2)
+const multiply_result = multiply(3,2)
+
+console.log(`SUM: ${sum_result}\nPRODUCT: ${multiply_result}`)
