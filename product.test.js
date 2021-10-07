90bc5cb48a0022868194d2faa0b68a4b065a3b72
Shahed Salehian <shahed.salehian@gmail.com>
2021-10-07 21:56:41 +0200

    Added more tests; remove split by timings

--- product.test.js
+++ product.test.js
@@ -1,5 +1,9 @@
 const product = require('./product');
 
-test('multiplies 1 * 2 to equal 2', () => {
-  expect(product(1, 2)).toBe(2);
-});
+test('product of 1 * 2 to equal 2', () => {
+  expect(product(1, 2)).toBe(2)
+})
+
+test('product of 1 * 2 to not equal 1', () => {
+  expect(product(1,2)).not.toBe(1)
+})
